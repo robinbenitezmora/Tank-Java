9e04f3928eeb40ebe96efb2cd5e21467c2860bed
robinbenitezmora <robinbenitez1990@gmail.com>
2024-02-13 19:37:55 -0500

    Create SimpleSoundPlayer.java class

---
+++ GameClient/dist/visual/src/SimpleSoundPlayer.java
@@ -0,0 +1,13 @@
+
+public class SimpleSoundPlayer {
+
+ public SimpleSoundPlayer(String string) {
+  // TODO Auto-generated constructor stub
+ }
+
+ public Object getSamples() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getSamples'");
+ }
+
+}

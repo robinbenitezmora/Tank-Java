264aeee403a113c3915e7ffe89aca3a0d24c055d
robinbenitezmora <robinbenitez1990@gmail.com>
2024-02-13 19:38:32 -0500

    Create GameBoardPanel.java

---
+++ GameClient/dist/visual/src/GameBoardPanel.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class GameBoardPanel {
+
+ public static ArrayList<Tank> getClients() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getClients'");
+ }
+
+}

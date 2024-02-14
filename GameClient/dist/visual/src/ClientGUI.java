6c8e23e9165511b5f3bcb0647dc4f1c2fef360c6
robinbenitezmora <robinbenitez1990@gmail.com>
2024-02-13 19:38:49 -0500

    Create  Client.java class

---
+++ GameClient/dist/visual/src/ClientGUI.java
@@ -0,0 +1,11 @@
+
+public class ClientGUI {
+
+ public Object gameStatusPanel;
+
+ public void setScore(int i) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'setScore'");
+ }
+
+}

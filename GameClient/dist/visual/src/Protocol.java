0a1f0b6117858088fcd704b87a02918554de71da
robinbenitezmora <robinbenitez1990@gmail.com>
2024-02-13 19:38:13 -0500

    Create Protocol.java class

---
+++ GameClient/dist/visual/src/Protocol.java
@@ -0,0 +1,9 @@
+
+public class Protocol {
+
+ public String RemoveClientPacket(Object tankID) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'RemoveClientPacket'");
+ }
+
+}

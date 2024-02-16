601318fc917a3b6dd6cf08b221af9b9d4199e6c1
robinbenitezmora <robinbenitez1990@gmail.com>
2024-02-15 19:02:52 -0500

    Add code in GameBoardPanel.java class

--- GameClient/dist/visual/src/GameBoardPanel.java
+++ GameClient/dist/visual/src/GameBoardPanel.java
@@ -7,4 +7,14 @@ public class GameBoardPanel {
   throw new UnsupportedOperationException("Unimplemented method 'getClients'");
  }
 
+ public void setGameStatus(boolean b) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'setGameStatus'");
+ }
+
+ public void setFocusable(boolean b) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'setFocusable'");
+ }
+
 }

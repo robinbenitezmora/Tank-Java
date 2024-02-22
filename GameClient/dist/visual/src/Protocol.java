10a01edb58f13ef9850b288447113851b2172c48
robinbenitezmora <robinbenitez1990@gmail.com>
2024-02-22 06:56:23 -0500

    Update Protocol.java class

--- GameClient/dist/visual/src/Protocol.java
+++ GameClient/dist/visual/src/Protocol.java
@@ -1,24 +1,32 @@
 
 public class Protocol {
+ private String message = "";
 
- public String RemoveClientPacket(Object tankID) {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'RemoveClientPacket'");
+ public Protocol() {
  }
 
- public String ExitMessagePacket(Object clientTankID) {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'ExitMessagePacket'");
+ public String RegisterPacket(int x, int y) {
+  message = "REGISTER " + x + " " + y;
+  return message;
  }
 
- public String UpdatePacket(int xPosition, int yPosition, int direction, Object tankID) {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'UpdatePacket'");
+ public String RemoveClientPacket(int id) {
+  message = "REMOVE " + id;
+  return message;
  }
 
- public String ShotPacket(Object tankID) {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'ShotPacket'");
+ public String ExitMessagePacket(int id) {
+  message = "EXIT " + id;
+  return message;
  }
 
+ public String UpdatePacket(int x, int y, int id, int dir) {
+  message = "UPDATE " + x + " " + y + " " + id + " " + dir;
+  return message;
+ }
+
+ public String ShotPacket(int id) {
+  message = "SHOT " + id;
+  return message;
+ }
 }

b1e7f257f0928b89f7b84472bff93a56bd44b25d
robinbenitezmora <robinbenitez1990@gmail.com>
2024-02-26 08:02:31 -0500

    Create BufferedImage.java class

--- GameClient/dist/visual/src/BufferedImage.java
+++ GameClient/dist/visual/src/BufferedImage.java
@@ -1,4 +1,16 @@
 
 public class BufferedImage {
 
+ public static final String TYPE_INT_ARGB = null;
+
+ public Graphics getGraphics() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getGraphics'");
+ }
+
+ public Graphics createGraphics() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'createGraphics'");
+ }
+
 }

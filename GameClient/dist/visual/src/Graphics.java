ff20189c3c38490004b91ec1539458b93f0203b6
robinbenitezmora <robinbenitez1990@gmail.com>
2024-02-26 08:02:45 -0500

    Create Graphics.java class

--- GameClient/dist/visual/src/Graphics.java
+++ GameClient/dist/visual/src/Graphics.java
@@ -1,4 +1,15 @@
+import java.awt.Image;
 
 public class Graphics {
+
+ public void dispose() {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'dispose'");
+ }
+
+ public void drawImage(Image image, int i, int j, Object object) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'drawImage'");
+ }
 
 }
